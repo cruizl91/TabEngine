b3adb1c6ca94ba3eb38ccdfa1525251aa571bba4
cruizl91 <carlor.ruiz@gmail.com>
2026-09-07 15:24:43 -0300

    fix: corregir nombre de paquete @vitejs/plugin-vue

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs.plugin-vue'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: './', // Asegura que los activos CSS y JS se encuentren correctamente en Vercel/GitHub Pages
+  base: './', // Asegura rutas relativas correctas en producción
 })
